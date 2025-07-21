b43a6d883af57142abc0901721c2f73a172d3e74
CordeiroZ <jvcordeiroc1@gmail.com>
2025-07-21 16:30:01 -0300

    Feat: LocaleTimeString

--- index.js
+++ index.js
@@ -29,8 +29,14 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
         weekday: "long"
      });
+     const data = new Date().toLocaleDateString("pt-BR")
+     const hora = new Date().toLocaleTimeString("pt-BR",{
+        hour: "numeric", minute: "numeric"
+     })
+
+     const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
     
-     console.log(diaDaSemana);
+     console.log(dataCompleta);
 
 })
 
