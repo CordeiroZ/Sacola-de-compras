c2916af785e3557af3eb116521eb705d37c52a87
CordeiroZ <jvcordeiroc1@gmail.com>
2025-07-21 17:35:03 -0300

    feat: adicionar data e hora aos itens da lista e separar função em arquivo

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-const inputItem = document.getElementById("input-item");
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
-let contador = 0; // Variável que vai trocar de valor, vai incrementar o valor
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault(); // Previne o comportamento padrão do botão ideal pra previnir envio de formulario se tiver algum erro
-    if (inputItem.value === "") {
-        alert("Por favor, insira um item!")
-        return
-    }
-
-    const itemDaLista = document.createElement("li");
-    const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox"
-    inputCheckbox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement("p");
-    nomeItem.innerText = inputItem.value;
-
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-
-    itemDaLista.appendChild(containerItemDaLista);
+    const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista);
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-        weekday: "long"
-     });
-     const data = new Date().toLocaleDateString("pt-BR")
-     const hora = new Date().toLocaleTimeString("pt-BR",{
-        hour: "numeric", minute: "numeric"
-     })
-
-     const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
-    
-     console.log(dataCompleta);
-
+    verificarListaVazia();
 })
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia () {
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if (itensDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block"
+    } else {
+        mensagemListaVazia.style.display = "none"
+        
+    }
+}
+
+verificarListaVazia();
+
 
 
 
